0c9c690d72ddd6f99bfcb8872f09bc0609f59956
cuicuidev <dmitryryzhenkov.dev@gmail.com>
2023-09-02 04:02:31 +0200

    it finally works, somewhat

--- .env.dev.sh
+++ .env.dev.sh
@@ -5,5 +5,3 @@ export POSTGRES_DB=database
 export POSTGRES_USER=root
 export POSTGRES_PASSWORD=debug
 export DJANGO_SECRET_KEY="django-insecure-*5-o#x+753^jm&0w@25pjim9h_w%bi=-j3b6x@4moye215*r-+"
-export RSA_PRIVATE_KEY_PATH=key
-export RSA_PUBLIC_KEY_PATH=key.pub
--- root/urls.py
+++ root/urls.py
@@ -1,4 +1,4 @@
-from django.contrib import admin
+# from django.contrib import admin
 from django.urls import path
 from django.urls import path
 from rest_framework_simplejwt.views import (
@@ -8,7 +8,7 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+#    path('admin/', admin.site.urls),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
