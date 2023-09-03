b9fe223773f21f17c798e7efc1cc6a9749a271b2
cuicuidev <dmitryryzhenkov.dev@gmail.com>
2023-09-03 03:44:55 +0200

    added basic crud routing for exercise entries

--- root/urls.py
+++ root/urls.py
@@ -1,6 +1,5 @@
-# from django.contrib import admin
-from django.urls import path
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -8,8 +7,11 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
-#    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+
+    path('', include('api.urls')),
+    
 ]
