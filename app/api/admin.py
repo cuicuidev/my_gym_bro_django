05835c3d444f8553778d05d3f246cbdd5f40e6a3
cuicuidev <dmitryryzhenkov.dev@gmail.com>
2023-09-06 03:55:43 +0200

    created basic models

--- app/api/admin.py
+++ app/api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+admin.site.register(models.Exercise)
+admin.site.register(models.SetEntry)
+admin.site.register(models.Workout)
+admin.site.register(models.Session)
